06b82fa427dfd64743429f19e435ac72c4a268a0
robin_delaporte <robin-delaporte@hotmail.fr>
2020-04-30 14:00:40 +0200

    removed async

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-///MAIN
+class Carousel {
+
+        /**
+         * 
+         * 
+         * @param {HTMLElement} element
+         * @param {Object} options
+         * @param {Object} options.slidesToScroll = Nombre d'elements à faire défiler
+         * @param {Object} options.slidesVisible = Nombre d'élements visibles dans un slide
+         */
+    
+    
+    constructor (element, option = {}) {
+        console.log("Hello")
+    }
+}
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    console.log(event)
+    new Carousel (document.querySelector('#carousel'), {
+        slidesToScroll: 3,
+        slidesVisible: 3
+    })
+})
